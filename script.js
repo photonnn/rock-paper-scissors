f3ba8c1443b6e926f3375cc4a3b93a3f343a8543
photonnn <wasjiri@gmail.com>
2022-04-27 14:25:13 +0200

    Add computerPlay function
    It chooses the weapon for the computer.

--- script.js
+++ script.js
@@ -4,4 +4,15 @@
 */
 
 
-console.log("Hello, World");
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    switch (randomNumber) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
+}
